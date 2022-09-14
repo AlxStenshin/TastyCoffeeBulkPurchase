70da7716da0bb7b14b0609959cf63bb3ac4342dd
AlxStenshin <alexander.stn@gmail.com>
2022-09-14 14:35:04 +0400

    Empty project init.

---
+++ src/main/java/ru/alxstn/tastycoffeebulkpurchase/TastyCoffeeBulkPurchaseApplication.java
@@ -0,0 +1,13 @@
+package ru.alxstn.tastycoffeebulkpurchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyCoffeeBulkPurchaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TastyCoffeeBulkPurchaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/alxstn/tastycoffeebulkpurchase/TastyCoffeeBulkPurchaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alxstn.tastycoffeebulkpurchase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastyCoffeeBulkPurchaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
