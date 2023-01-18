36abcf94d99d0fcf2db98303493189ca3d0a517e
AlxStenshin <alexander.stn@gmail.com>
2023-01-18 10:56:42 +0400

    Decimal formatter added for session summary message

--- src/main/java/ru/alxstn/tastycoffeebulkpurchase/service/BasicSessionSummaryMessageCreatorService.java
+++ src/main/java/ru/alxstn/tastycoffeebulkpurchase/service/BasicSessionSummaryMessageCreatorService.java
@@ -7,6 +7,7 @@ import ru.alxstn.tastycoffeebulkpurchase.entity.Session;
 import ru.alxstn.tastycoffeebulkpurchase.service.repositoryManager.PaymentManagerService;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.time.format.DateTimeFormatter;
 
 @Service
@@ -23,16 +24,18 @@ public class BasicSessionSummaryMessageCreatorService implements SessionSummaryM
     public String createSessionSummaryMessage(Session session) {
         logger.info("Building session summary.");
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        DecimalFormat decimalFormat = new DecimalFormat("#.00");
+
         return (session.isClosed() ? "\n<b>! Сессия закрыта. Заказы не принимаются. !</b>\n" : "") +
                 "<code>" +
                 "\nНаименование: " + session.getTitle() +
                 "\nТекущая скидка: " + session.getDiscountPercentage() + "%" +
-                "\nТоваров по скидке в закупке: " + session.getDiscountableWeight() + "кг." +
-                "\nКофе в закупке: " + session.getCoffeeWeight() + "кг." +
-                "\nЧая в закупке: " + session.getTeaWeight() + "кг." + "\n" +
-                "\nДата открытия: " + session.getDateTimeOpened().format(formatter) +
-                "\nДата закрытия: " + session.getDateTimeClosed().format(formatter) +
+                "\nТоваров по скидке в закупке: " + decimalFormat.format(session.getDiscountableWeight()) + "кг." +
+                "\nКофе в закупке: " + decimalFormat.format(session.getCoffeeWeight()) + "кг." +
+                "\nЧая в закупке: " + decimalFormat.format(session.getTeaWeight()) + "кг." + "\n" +
+                "\nДата открытия: " + session.getDateTimeOpened().format(dateFormatter) +
+                "\nДата закрытия: " + session.getDateTimeClosed().format(dateFormatter) +
                 "\nОплата: " + session.getPaymentInstruction() + "\n" +
                 "\nОбщая стоимость без скидки: " +  paymentManagerService.getSessionTotalPrice(session).orElse(new BigDecimal(0)) +  "₽" +
                 "\nОбщая стоимость со скидкой: " + paymentManagerService.getSessionTotalPriceWithDiscount(session).orElse(new BigDecimal(0)) + "₽" +
